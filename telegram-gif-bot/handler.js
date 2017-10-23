216b8c67ac675f6ed876e347061f0d328c3f6639
Eric Stoekl <ems5311@gmail.com>
2017-10-22 22:20:57 -0700

    use an env var for bot token. Send gifObj as payload to gif-maker

--- telegram-gif-bot/handler.js
+++ telegram-gif-bot/handler.js
@@ -4,7 +4,7 @@ const FaaS = require('openfaas')
 const faas = FaaS('http://gateway:8080')
 var TelegramBot = require('node-telegram-bot-api');
 
-var telegramBotToken = ''; // Add your own here
+var telegramBotToken = env.BOT_TOKEN; // Add your own here
 var telegramBot = new TelegramBot(telegramBotToken, {polling: false});    
 
 var request = require('request');
@@ -16,6 +16,16 @@ module.exports = function(context, callback) {
     var text = fullContext.message.text;
     console.log("created vars");
 
+    // Split text into array (separated by spaces)
+    // arr[1] is the start time, arr[2] is the duration
+    // Create a new object, send with faas.invoke
+    var arr = text.split(" ");
+    var gifObj = {};
+    if (arr.length > 1) {
+        gifObj.startTime = arr[1];
+        gifObj.duration = arr[2];
+    }
+
     faas.invoke(
         'youtube-dl', // function name
         text, // data to send to function
@@ -25,9 +35,11 @@ module.exports = function(context, callback) {
     .then(x => {
       telegramBot.sendMessage(chatId, "Got the video file...");
 
+      gifObj.body = x.body;
+
       faas.invoke(
         'gif-maker',
-        x.body,
+        gifObj,
         false,
         true
       )
