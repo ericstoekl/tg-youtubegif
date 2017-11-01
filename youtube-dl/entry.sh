3b085154a649dd5dd26355acbdf65fc4bc4a0fed
ericstoekl <ems5311@gmail.com>
2017-11-01 05:28:20 +0000

    get youtube-dl to work

--- youtube-dl/entry.sh
+++ youtube-dl/entry.sh
@@ -7,6 +7,9 @@ do
   echo "$line"
 done < "${1:-/dev/stdin}"
 
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' $line --no-warnings --quiet -o $nano.mp4
+youtube-dl -f 'bestvideo[ext=mp4]' $line --no-warnings --quiet -o $nano.mp4
 cat $nano.mp4 > /dev/stdout \
   && rm $nano.mp4
+
+
+#youtube-dl $line --no-warnings --quiet -o -
