161d8a24ce5af50471a0a1da819e478f0c186b37
Ubuntu <ubuntu@ip-172-31-30-60.us-west-2.compute.internal>
2017-11-12 23:58:35 +0000

    update fwatchdog for gif-maker add timeouts

--- telegram-gif-bot/handler.js
+++ telegram-gif-bot/handler.js
@@ -13,16 +13,6 @@ module.exports = function(context, callback) {
     var text = fullContext.message.text;
     console.log("created vars");
 
-    // Split text into array (separated by spaces)
-    // arr[1] is the start time, arr[2] is the duration
-    // Create a new object, send with faas.invoke
-    var arr = text.split(" ");
-    var gifObj = {};
-    if (arr.length > 1) {
-        gifObj.startTime = arr[1];
-        gifObj.duration = arr[2];
-    }
-
     faas.invoke(
         'youtube-dl', // function name
         text, // data to send to function
@@ -32,11 +22,9 @@ module.exports = function(context, callback) {
     .then(x => {
       telegramBot.sendMessage(chatId, "Got the video file...");
 
-      gifObj.body = x.body;
-
       faas.invoke(
         'gif-maker',
-        JSON.stringify(gifObj),
+        x.body,
         false,
         true
       )
