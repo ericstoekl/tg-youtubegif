3740a03c87a3d35bf758cf8a771d2719e0f7a870
ericstoekl <ems5311@gmail.com>
2017-11-13 00:11:10 +0000

    update tg-gif-bot/handler

--- telegram-gif-bot/handler.js
+++ telegram-gif-bot/handler.js
@@ -1,58 +1,45 @@
 "use strict"
 
+const fs = require('fs');
 const FaaS = require('openfaas')
 const faas = FaaS('http://gateway:8080')
 var TelegramBot = require('node-telegram-bot-api');
 
-var telegramBotToken = "";
-fs.readFile('/run/secrets/BOT_TOKEN', 'utf8', (err, data) => {
-  if (err) throw err;
-  telegramBotToken = data;
-})
-console.log(telegramBotToken)
+var telegramBotToken = fs.readFileSync('/run/secrets/BOT_TOKEN', 'utf8').trim();
+console.log("1. \"" + telegramBotToken + "\"")
 var telegramBot = new TelegramBot(telegramBotToken, {polling: false});    
 
 module.exports = function(context, callback) {
-    var fullContext = JSON.parse(context);
-    var chatId = fullContext.message.chat.id;
-    var text = fullContext.message.text;
-    console.log("created vars");
 
-    // Split text into array (separated by spaces)
-    // arr[1] is the start time, arr[2] is the duration
-    // Create a new object, send with faas.invoke
-    var arr = text.split(" ");
-    var gifObj = {};
-    if (arr.length > 1) {
-        gifObj.startTime = arr[1];
-        gifObj.duration = arr[2];
-    }
+  
+  var fullContext = JSON.parse(context);
+  var chatId = fullContext.message.chat.id;
+  var text = fullContext.message.text;
+
+  faas.invoke(
+      'youtube-dl', // function name
+      text, // data to send to function
+      false,
+      true
+  )
+  .then(x => {
+    telegramBot.sendMessage(chatId, "Got the video file...");
 
     faas.invoke(
-        'youtube-dl', // function name
-        text, // data to send to function
-        false,
-        true
+      'gif-maker',
+      x.body,
+      false,
+      true
     )
-    .then(x => {
-      telegramBot.sendMessage(chatId, "Got the video file...");
-
-      gifObj.body = x.body;
-
-      faas.invoke(
-        'gif-maker',
-        JSON.stringify(gifObj),
-        false,
-        true
-      )
-      .then( y => {
-        telegramBot.sendMessage(chatId, "Got the gif file...");
-        telegramBot.sendDocument(chatId, y.body);
-      })
-      .catch(err => console.log(err));
-
+    .then( y => {
+      telegramBot.sendMessage(chatId, "Got the gif file...");
+      telegramBot.sendDocument(chatId, y.body);
     })
     .catch(err => console.log(err));
 
-    callback(undefined, {status: true});
+  })
+  .catch(err => console.log(err));
+
+
+  callback(undefined, {status: "done"});
 }
