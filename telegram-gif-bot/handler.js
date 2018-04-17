03786126ac3fec7580cd0fb59ed9b268a4244854
Eric Stoekl <ems5311@gmail.com>
2018-04-17 01:11:46 +0000

    simplify node code
    Signed-off-by: Eric Stoekl <ems5311@gmail.com>

--- telegram-gif-bot/handler.js
+++ telegram-gif-bot/handler.js
@@ -2,25 +2,16 @@
 
 const FaaS = require('openfaas')
 const faas = FaaS('http://gateway:8080')
-var TelegramBot = require('node-telegram-bot-api');
-
-var telegramBotToken = process.env.BOT_TOKEN;
-var telegramBot = new TelegramBot(telegramBotToken, {polling: false});    
 
 module.exports = function(context, callback) {
-    var fullContext = JSON.parse(context);
-    var chatId = fullContext.message.chat.id;
-    var text = fullContext.message.text;
-    console.log("created vars");
 
     faas.invoke(
         'youtube-dl', // function name
-        text, // data to send to function
+        context, // data to send to function
         false,
         true
     )
     .then(x => {
-      telegramBot.sendMessage(chatId, "Got the video file...");
 
       faas.invoke(
         'gif-maker',
@@ -29,13 +20,12 @@ module.exports = function(context, callback) {
         true
       )
       .then( y => {
-        telegramBot.sendMessage(chatId, "Got the gif file...");
-        telegramBot.sendDocument(chatId, y.body);
+        process.stdout.write(y.body);
       })
       .catch(err => console.log(err));
 
     })
     .catch(err => console.log(err));
 
-    callback(undefined, {status: true});
+    //callback(undefined, {status: true});
 }
